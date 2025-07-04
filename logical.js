e4d378c7f414af2a4d7213f9bf723b9099e6b153
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-04 10:19:06 +0530

    operators

---
+++ logical.js
@@ -0,0 +1,11 @@
+let num=199
+console.log(num>=100 && num<=200)
+
+
+let num1=499
+if(num1>=100 && num1<=999){
+    console.log("yes given number is 3digit number")
+}
+else{
+    console.log("not a 3digit number")
+}
---
+++ relational.js
@@ -0,0 +1,10 @@
+let sal=45000;
+console.log(sal>40000)
+console.log(sal==40000)
+let sal1=50000;
+if(sal1>=45000){
+    console.log("eligible")
+}
+else{
+    console.log("not eligible")
+}
