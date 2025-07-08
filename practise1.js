03cd4914085c9e67fe1ed9cc15051b487b521ea0
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-08 11:21:32 +0530

    loops

---
+++ practise1.js
@@ -0,0 +1,34 @@
+const people = [
+{ name: "su", gender: "male" },
+ { name: "lice", gender: "female" },
+{ name: "ob", gender: "male" },
+{ name: "ra", gender: "female" }
+];
+
+const malePeople = people.filter(person => person.gender === "male");
+malePeople.forEach(person => {
+console.log(person);
+});
+
+
+
+
+var enames = ["s","u","r","e"];
+for (let i=0;i<=3;i++){
+    console.log(enames[i]);
+}
+var enames = ["s","u","r","e"];
+for (var i=0;i<enames.length;i++){
+    console.log(enames[i]);
+}
+var enames = [
+    {eid:1, ename: "ce", salary: 50000},
+{eid:2,ename:"ob",salary:60000},
+{eid:3,ename:"ie",salary:70000}
+];
+for (emp of enames){
+    console.log(emp.ename);
+    console.log(emp.eid);
+}
+
+
