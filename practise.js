e53257a95f47d213953cec85de1d55161c2f253a
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-09 12:03:13 +0530

    examples

--- practise.js
+++ practise.js
@@ -6,3 +6,23 @@ console.log(a===c)
 console.log(a&b)
 console.log(a!=b)
 console.log(a=b)
+
+
+
+
+for(i=1;i<=10;i++){
+    console.log("9*",i,"=",9*i)
+}
+
+
+
+for(i=1;i<=10;i++){
+    console.log(`9*${i}=${9*i}`)
+}
+
+i=1;
+while(i<=10)
+{
+console.log(`10*${i}=${10*i}`)
+i++;
+}
