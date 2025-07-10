4ac2425acc3c80a94a0d7c4f94b46e42b5be7fa8
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-10 10:39:48 +0530

    arrays

---
+++ objects.js
@@ -0,0 +1,22 @@
+let emp=[12,34,56,78,90]
+let eid=[1,2,3,4,5,6,7]
+let ename=[]
+
+console.log(emp)
+for (let em of emp){
+    console.log(em)
+}
+console.log(emp[2])
+console.log(eid[1])
+if(emp.length>0){
+    console.log("not empty array")
+}
+else{
+    console.log("empty array")
+}
+if(ename.length>0){
+    console.log("not empty array")
+}
+else{
+    console.log("empty array")
+}
