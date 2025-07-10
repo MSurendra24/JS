653be2fe7b153c17470f99c4759a0f2d2f6da879
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-10 10:51:51 +0530

    array

---
+++ objects.js
@@ -0,0 +1,18 @@
+let emp={
+    ename:"sure",
+    eid:12,
+    enum:79,
+}
+console.log(emp)
+
+for(em in emp){
+    console.log(em.ename)
+}
+console.log(Object.keys(emp))
+console.log(Object.keys(emp).length>0)
+if(Object.keys(emp).length>0){
+    console.log("not empty")
+}
+else{
+    console.log("empty")
+}
