6e041dfbbb5e29c15c4772acf4bad91ecf649a58
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-16 19:22:37 +0530

    task1

---
+++ functions/methods/two.js
@@ -0,0 +1,8 @@
+one=()=>{
+document.getElementsByTagName("button")[0].innerHTML="hii";
+document.getElementsByTagName("button")[0].style.backgroundColor="rgba(195, 240, 117, 1)"
+}
+two=()=>{
+document.getElementsByTagName("button")[0].innerHTML="hello";
+document.getElementsByTagName("button")[0].style.backgroundColor="rgba(217, 92, 92, 1)"
+}
