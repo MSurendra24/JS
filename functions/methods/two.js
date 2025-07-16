965f259c6ef68cffa6255acc0348526215673cf7
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-16 19:35:05 +0530

    tasks2

--- functions/methods/two.js
+++ functions/methods/two.js
@@ -3,6 +3,10 @@ document.getElementsByTagName("button")[0].innerHTML="hii";
 document.getElementsByTagName("button")[0].style.backgroundColor="rgba(195, 240, 117, 1)"
 }
 two=()=>{
-document.getElementsByTagName("button")[0].innerHTML="hello";
-document.getElementsByTagName("button")[0].style.backgroundColor="rgba(217, 92, 92, 1)"
+document.getElementsByTagName("button")[1].innerHTML="hello";
+document.getElementById("btn2").style.backgroundColor="rgba(217, 92, 92, 1)"
 }
+function three(){
+document.getElementById("btn3").style.backgroundColor="rgba(118, 143, 243, 1)"
+}
+three();
