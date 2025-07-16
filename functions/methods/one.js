d23488fef42f4fb9c1b50897ccdca7a80991613e
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-16 12:04:00 +0530

    methods

---
+++ functions/methods/one.js
@@ -0,0 +1,15 @@
+let fun=()=>{
+    document.getElementsByTagName("button")[0].style.backgroundColor='rgba(255, 0, 0, 1)'
+}
+let fun1=()=>{
+    document.getElementsByTagName("button")[1].style.backgroundColor='rgba(253, 253, 253, 1)'
+}
+let fun2=()=>{
+    document.getElementsByTagName("button")[1].style.backgroundColor='rgba(63, 230, 21, 1)'
+}
+let fun3=()=>{
+    document.getElementsByTagName("button")[0].style.backgroundColor='rgba(50, 36, 203, 1)'
+}
+const input = document.getElementById("blurInput");
+
+input.value = input.value.toUpperCase();
