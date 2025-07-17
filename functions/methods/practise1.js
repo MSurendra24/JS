fee221bdf3b289e2e0db933aea1fb564ccf429ae
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-17 12:40:32 +0530

    methods

--- functions/methods/practise1.js
+++ functions/methods/practise1.js
@@ -7,9 +7,17 @@ hi=()=>{
 hii=()=>{
     document.getElementById("h3").style.backgroundColor="rgba(216, 65, 174, 1)"
 }
-hiii=()=>{
-    document.getElementsByTagName('button')[4].style.backgroundColor="rgba(233, 134, 34, 1)"
+hii2=()=>{
+    document.getElementsByTagName('button')[3].style.backgroundColor="rgba(233, 134, 34, 1)"
 }
 hi1=()=>{
     document.getElementById('h5').style.backgroundColor="rgba(30, 197, 197, 1)"
+}
+one=()=>{
+    let name=document.getElementById("msg").value;
+    document.getElementById("msg").value=name.toLowerCase();
+}
+two=()=>{
+    let name=document.getElementsByTagName("input")[1].value;
+    document.getElementsByTagName("input")[1].value=name.toUpperCase();
 }
